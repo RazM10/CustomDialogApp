b04e517edfbd23243d1bb1606ce55992dcebfd1b
Razaul Munir <razaulmunir@gmail.com>
2019-12-21 12:54:50 +0600

    set up dialog in Activity

--- app/src/main/java/org/myself/customalertdialog/CustomDialog.java
+++ app/src/main/java/org/myself/customalertdialog/CustomDialog.java
@@ -1,14 +1,27 @@
 package org.myself.customalertdialog;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
-public class CustomDialog extends AppCompatActivity {
+public class CustomDialog extends Dialog implements android.view.View.OnClickListener{
+
+    public CustomDialog(@NonNull Context context) {
+        super(context);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_dialog);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
